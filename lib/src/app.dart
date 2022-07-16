ceba56cb310e60b856d804afe7fe5bee51cb4cf5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-16 18:55:41 +0200

    basic form implimented

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import './screens/login.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Log me in',
       home: Scaffold(
-        body: Text('Show a form here'),
+        body: LoginScreen(),
       ),
     );
   }
