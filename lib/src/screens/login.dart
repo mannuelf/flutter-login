55805d2a042893b0d86e47768b8669fa1cda0132
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-16 18:49:49 +0200

    login screen stateful widget boilerplae

---
+++ lib/src/screens/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build() {}
+}
